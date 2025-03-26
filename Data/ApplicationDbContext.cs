9516a2ac15f75da31ddaf11eef5b1ffc8f25feaa
rickysanchez1 <richard.sanchez1983@gmail.com>
2025-03-25 22:53:39 -0700

    Adding solution and dbcontext model

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+    {            
+    }
+        // Defining dbsets, tables in the DB
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
