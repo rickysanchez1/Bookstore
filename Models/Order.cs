26c6371304fa16d99fd7163532b332858f2ef051
rickysanchez1 <richard.sanchez1983@gmail.com>
2025-03-28 15:11:24 -0700

    Added Order and User entity

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookstore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+        public User User { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,12 @@
 ﻿namespace Bookstore.Models
+
 {
     public class User
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
